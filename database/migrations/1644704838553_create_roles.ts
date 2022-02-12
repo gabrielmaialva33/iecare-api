ad4451eb840243a65260cef72dc1ce01190311ad
kenzoYmagawa <alvaro.ymagawa@yazo.com.br>
2022-02-12 19:35:35 -0300

    feat: make roles migration

---
+++ database/migrations/1644704838553_create_roles.ts
@@ -0,0 +1,26 @@
+import BaseSchema from '@ioc:Adonis/Lucid/Schema'
+import Logger from '@ioc:Adonis/Core/Logger'
+
+export default class Roles extends BaseSchema {
+  protected tableName = 'roles'
+
+  public async up() {
+    if (!(await this.schema.hasTable(this.tableName)))
+      this.schema.createTable(this.tableName, (table) => {
+        table.uuid('id').primary().defaultTo(this.db.rawQuery('uuid_generate_v4()').knexQuery)
+
+        table.string('slug', 100).notNullable()
+        table.string('name', 100).notNullable().unique()
+        table.string('description', 100).notNullable()
+        table.jsonb('permissions').defaultTo('{}')
+
+        table.timestamp('created_at', { useTz: true })
+        table.timestamp('updated_at', { useTz: true })
+      })
+    else Logger.info('Roles migration already running')
+  }
+
+  public async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
