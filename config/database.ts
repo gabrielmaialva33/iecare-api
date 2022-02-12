7bcbaaa4fd60820f2d8cd4d69d7c92bb177483ae
kenzoYmagawa <alvaro.ymagawa@yazo.com.br>
2022-02-12 18:31:26 -0300

    chore: conguture database

--- config/database.ts
+++ config/database.ts
@@ -36,20 +36,19 @@ const databaseConfig: DatabaseConfig = {
     pg: {
       client: 'pg',
       connection: {
-        host: Env.get('PG_HOST'),
-        port: Env.get('PG_PORT'),
-        user: Env.get('PG_USER'),
-        password: Env.get('PG_PASSWORD', ''),
-        database: Env.get('PG_DB_NAME'),
+        host: Env.get('PG_HOST', 'localhost'),
+        port: Env.get('PG_PORT', 5432),
+        user: Env.get('PG_USER', 'postgres'),
+        password: Env.get('PG_PASSWORD', 'postgres'),
+        database: Env.get('PG_DB_NAME', 'postgres'),
       },
       migrations: {
         naturalSort: true,
       },
-      healthCheck: false,
+      healthCheck: true,
       debug: false,
     },
-
-  }
+  },
 }
 
 export default databaseConfig
