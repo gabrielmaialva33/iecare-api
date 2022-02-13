1af45698980a08cff8dbc86357db8b83d78d7c12
kenzoYmagawa <alvaro.ymagawa@yazo.com.br>
2022-02-13 02:00:21 -0300

    fix: remove middleware on create user

--- app/Modules/User/Routes/UserRoutes.ts
+++ app/Modules/User/Routes/UserRoutes.ts
@@ -3,9 +3,9 @@ import UsersController from 'App/Modules/User/Controllers/Http/User/UsersControl
 import AuthController from 'App/Modules/User/Controllers/Http/User/AuthController'
 
 Route.post('login', new AuthController().login).as('auth.login')
+Route.post('/users', new UsersController().create).as('user.create')
 
 Route.group(() => {
   Route.get('/users/:id', new UsersController().show).as('user.show')
-  Route.post('/users', new UsersController().create).as('user.create')
   Route.put('/users/:id', new UsersController().update).as('user.update')
 }).middleware(['auth', 'acl:root,admin,user,guest'])
