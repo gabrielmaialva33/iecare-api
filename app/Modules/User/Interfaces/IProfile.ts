0905f846c3044dcea30326e9752e5c374ff80ffb
kenzoYmagawa <alvaro.ymagawa@yazo.com.br>
2022-02-13 17:54:19 -0300

    feat: make profile repository

--- app/Modules/User/Interfaces/IProfile.ts
+++ app/Modules/User/Interfaces/IProfile.ts
@@ -1,11 +1,34 @@
+import Profile from 'App/Modules/User/Models/Profile'
+
 export namespace IProfile {
-  export interface Repository {}
+  export interface Repository {
+    show(profileId: string): Promise<Profile | null>
+
+    create(data: DTO.Store): Promise<Profile>
+
+    update(profile: Profile): Promise<Profile>
+  }
 
   export interface Helpers {}
 
   export namespace DTO {
-    export interface Store {}
+    export interface Store {
+      avatarUrl?: string
+      cellphone: string
+      cpf: string
+      rg?: string
+      company: string
+      role: string
+      userId: string
+    }
 
-    export interface Update {}
+    export interface Update {
+      avatarUrl?: string
+      cellphone?: string
+      cpf?: string
+      rg?: string
+      company?: string
+      role?: string
+    }
   }
 }
--- app/Modules/User/Repositories/ProfilesRepository.ts
+++ app/Modules/User/Repositories/ProfilesRepository.ts
@@ -1,3 +1,28 @@
 import { IProfile } from 'App/Modules/User/Interfaces/IProfile'
+import Profile from 'App/Modules/User/Models/Profile'
 
-export default class ProfilesRepository implements IProfile.Repository {}
+export default class ProfilesRepository implements IProfile.Repository {
+  private orm: typeof Profile
+
+  constructor() {
+    this.orm = Profile
+  }
+  /**
+   * Repository
+   */
+  public async show(profileId: string): Promise<Profile | null> {
+    return this.orm.query().where({ id: profileId }).first()
+  }
+
+  public async create(data: IProfile.DTO.Store): Promise<Profile> {
+    return this.orm.create(data)
+  }
+
+  public async update(profile: Profile): Promise<Profile> {
+    return profile.save()
+  }
+
+  /**
+   * Helpers
+   */
+}
