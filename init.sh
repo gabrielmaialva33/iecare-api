2a83d2291ddbbfba062d6ab443fbae758ee43667
Gabriel Maia <gabrielmaialva33@gmail.com>
2022-11-06 09:56:05 -0300

    fix: git workflow

--- init.sh
+++ init.sh
@@ -1,1 +1,22 @@
-nohup bash -c "bin/iecare" > /dev/null 2>&1 & echo $! > iecare.pid
+if [ ! -f /etc/systemd/system/iecare.service ]
+            then
+              echo "[Unit]
+              Description=iecare
+              After=network.target
+
+              [Service]
+              User=ubuntu
+              WorkingDirectory=/home/ubuntu/iecare-api
+              ExecStart=/home/ubuntu/iecare-api/bin/iecare
+              Restart=always
+              RestartSec=3
+              StartLimitInterval=0
+
+              [Install]
+              WantedBy=multi-user.target" > /etc/systemd/system/iecare.service
+            fi
+            sudo systemctl daemon-reload
+            sudo systemctl enable iecare.service
+            sudo systemctl start iecare.service
+
+
